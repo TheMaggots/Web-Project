4880d02bf516aa971331045b6b0dd550358b1941
Siddharth <marshtomp.1999@hotmail.com>
2020-04-21 19:25:26 +0530

    Added basic templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Siddharth'}
+    posts = [
+        {
+            'author': {'username': 'Akash'},
+            'body': 'Beautiful day playing DOTA'
+        },
+        {
+            'author': {'username': 'Avinash'},
+            'body': 'Joker movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
