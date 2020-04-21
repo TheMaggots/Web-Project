d6eec5399d39da084b1b64ecdab863ff47eb256b
Siddharth <marshtomp.1999@hotmail.com>
2020-04-21 20:16:23 +0530

    Error occured in login.html

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,18 +1,10 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Siddharth'}
-    posts = [
-        {
-            'author': {'username': 'Akash'},
-            'body': 'Beautiful day playing DOTA'
-        },
-        {
-            'author': {'username': 'Avinash'},
-            'body': 'Joker movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
