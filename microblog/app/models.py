ac73f4e8599109f6ee42ec8f9e555e436751a8a5
Siddharth <marshtomp.1999@hotmail.com>
2020-04-22 16:21:17 +0530

    User avatar and Login

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -3,6 +3,8 @@ from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
+from hashlib import md5
+
 #Created a model for the User and Post and user mixin class
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +12,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 #Defines what object it is using the command prompt or terminal
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -19,6 +23,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+#avatar profile 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+
 #Flask-login user loader function
 @login.user_loader
 def load_user(id):
